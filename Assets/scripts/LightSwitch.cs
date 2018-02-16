8357ec2483e1b1777349400cef9cdd29b8f9ce80
Kenny Wardlaw <kwardlaw3@gatech.edu>
2018-02-16 17:58:04 -0500

    Update

--- Assets/scripts/LightSwitch.cs
+++ Assets/scripts/LightSwitch.cs
@@ -5,14 +5,14 @@
 
     public class LightSwitch : MonoBehaviour
     {
-        public GameObject gameObj;
+        public GameObject lightswitchObj;
         // Update is called once per frame
         void Update()
         {
-            if (this.gameObj != null)
+            if (this.lightswitchObj != null)
             {
                 if (Input.GetKeyUp(KeyCode.Space))
-                    this.gameObj.SetActive(!this.gameObj.activeSelf);
+                    this.lightswitchObj.SetActive(!this.lightswitchObj.activeSelf);
             }
         }
     }
